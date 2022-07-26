07fc71f488055b4b3b9a4748515cd9d92678844e
Mahabobur Rahman <mahaboburrahman866@gmail.com>
2022-07-26 18:18:41 +0600

    ss

--- react_leaflet/src/MapDraw.jsx
+++ react_leaflet/src/MapDraw.jsx
@@ -14,7 +14,44 @@ L.Icon.Default.mergeOptions({
   shadowUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
 });
-
+data=[
+  {
+    "id": 137,
+    "latlngs": {
+      "0": {
+        "lat": 51.528109772725664,
+        "lng": -0.1762962341308594
+      },
+      "1": {
+        "lat": 51.53630135943925,
+        "lng": -0.15209197998046875
+      },
+      "2": {
+        "lat": 51.49891002104616,
+        "lng": -0.14659881591796878
+      },
+      "3": {
+        "lat": 51.51785410519205,
+        "lng": -0.09904861450195314
+      },
+      "4": {
+        "lat": 51.51336652433144,
+        "lng": -0.07123947143554689
+      },
+      "5": {
+        "lat": 51.49049436552779,
+        "lng": -0.05544662475585938
+      },
+      "6": {
+        "lat": 51.486431753930475,
+        "lng": -0.08428573608398438
+      }
+    },
+    "color": "#3ab569",
+    "color_opacity": 0.5,
+    "color_width": 4,
+    "smoothFactor": 1
+  }]
 const MapDrawer = () => {
   const [center, setCenter] = useState([51.505, -0.09]);
   const [mapLayers, setMapLayers] = useState([]);
@@ -23,7 +60,7 @@ const MapDrawer = () => {
   const mapRef = useRef();
   const _onCreated = (e) => {
     console.log(e);
-    const { layerType, layer } = e;
+    const { layerType, layer } = data;
     if (layerType === "polyline") {
       const { _leaflet_id } = layer;
       setMapLayers((layers) => [
